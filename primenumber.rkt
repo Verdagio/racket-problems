05af4a8c3fe0eb17898d308befcf4954c5051b5f
Verdagio <dverdagio@gmail.com>
2018-01-29 12:48:38 +0000

    minor adjustments

--- primenumber.rkt
+++ primenumber.rkt
@@ -5,24 +5,24 @@
 ;Call the function decide-prime.
 
 ; check prime takes a number -> returns boolean
-(define (decide-prime n)
-  (recursion n 2))
-
 ;1: 1 is not a prime
 ;2: 2 is but anything else divisible by 2 is not!
 ;3: if m is greater than the square root of n, return otherwise continue
 ;4: if the remainder of n / m is 0 its not prime 
 ;5: brute force recursively checking each m against n
-(define (recursion n m)
-    (if (= n 1) ; 1
+(define m 2)
+(define (decide-prime n)
+      (if (= n 1) ; 1
         #f
         (if (= n 2) ; 2 
             #t
-            (if (> m (integer-sqrt n)) ; 3
-                #t
-                (if (= (remainder n m)0) ; 4
-                    #f
-                    (recursion n (+ m 1))))))); 5
+            (recursion n m))))
+(define (recursion n m)
+  (if (> m (integer-sqrt n)) ; 3
+      #t
+      (if (= (remainder n m)0) ; 4
+          #f
+          (recursion n (+ m 1))))); 5
               
 (decide-prime 4) ; #f
 (decide-prime 7) ; #t
