00d85b3a2be4f99590ab484edd512fcf3ac1b430
Verdagio <dverdagio@gmail.com>
2018-02-07 13:46:24 +0000

    Added alternate methods of completing tasks

--- collatsConjecture.rkt
+++ collatsConjecture.rkt
@@ -11,15 +11,25 @@
 ; collatz-list : takes a positive number -> returns list
 
 ; 1: if n is 1 no need to continue
-; 2: if n is an even number add n/2 to the list
-; The primitive cons takes two arguments. The second argument to cons should be a list, but doesn't always have to be. The result is a list.
+; 2: if n is an even number add (n/2) to the list
+; The primitive cons takes two arguments. The second argument to cons should be a list
+; but doesn't always have to be. The result is a list.
 ; In the Little schemer the second commandment is: Use cons to build lists!
 ; 3: recursive call function with n as arguement
-; 4: otherwise if the number is odd add n*3 + 1 to the list
+; 4: otherwise if the number is odd add (n*3 + 1) to the list
 
-(define ls '())
-
+; manual checking
 (define (collatz-list n)
+  (cond
+    ((= n 1) '(1)); 1
+    ((= (modulo n 2) 0) ; 2
+      (cons n (collatz-list (/ n 2)))); 3
+    ((= (modulo n 2) 1) ; 4
+      (cons n (collatz-list (+ (* 3 n) 1)))))); 3
+
+; using a pre-defined list and the even? odd? check
+(define ls '())
+(define (collatz-listv2 n)
   (append ls n)
   (cond
     ((= n 1) ls '(1)); 1
@@ -29,5 +39,12 @@
       (cons n (collatz-list (+ (* 3 n) 1)))))); 3
 
 
+
+; expected both should return the same result
 (collatz-list 7)
+(collatz-listv2 7)
 (collatz-list 45)
+(collatz-listv2 45)
+(collatz-list 51)
+(collatz-listv2 51)
+
--- primenumber.rkt
+++ primenumber.rkt
@@ -7,26 +7,50 @@
 ; check prime takes a number -> returns boolean
 ;1: 1 & 0 are not prime numbers and we can only check non-negative numbers
 ;2: check recursively through every number in range m -> n
-;3: if m is greater than the square root of n, return otherwise continue
+;3 - optimal : if m is greater than the square root of n, return otherwise continue
      ; We use the square root of n to reduce the complexity of the algorithm
      ; This goes against the whole idea brute force but is a good solution if it weren't a brute force task
      ; https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
-;3 - alt: check if m is equal to n - this will check against every number in the range of m -> n
+;3 - expensive (brute force): check if m is equal to n - this will check against every number in the range of m -> n
 ;4: if the remainder of n / m is 0 its not prime 
 ;5: brute force recursively checking each m against n
 (define m 2)
+
+; brute force method (check everything along the way)
 (define (decide-prime n)
       (if (< n 2) ; 1
         #f
         (recursion n m))); 2
+
 (define (recursion n m)
-  ;(if (> m (integer-sqrt n)) ; 3
-   (if (= m n); 3 - alt
+   (if (= m n); 3 - expensive (brute force)
+      #t
+      (if (= (modulo n m)0) ; 4
+          #f
+          (recursion n (+ m 1))))); 5
+
+; optimal (check only while m is greater than the square root of n)
+(define (decide-prime-optimal n)
+      (if (< n 2) ; 1
+        #f
+        (recursion-optimized n m))); 2
+
+(define (recursion-optimized n m)
+  (if (> m (integer-sqrt n)) ; 3 (optimal)
       #t
       (if (= (remainder n m)0) ; 4
           #f
-          (recursion n (+ m 1))))); 5
-              
+          (recursion-optimized n (+ m 1))))); 5
+
+(decide-prime-optimal 4) ; #f
+(decide-prime-optimal 7) ; #t
+(decide-prime-optimal 37) ; #t
+(decide-prime-optimal 141) ; #f
+(decide-prime-optimal 51) ; #f
+(decide-prime-optimal 5) ; #t
+(decide-prime-optimal 3) ; #t
+(decide-prime-optimal 1) ; #f
+
 (decide-prime 4) ; #f
 (decide-prime 7) ; #t
 (decide-prime 37) ; #t
@@ -35,8 +59,6 @@
 (decide-prime 5) ; #t
 (decide-prime 3) ; #t
 (decide-prime 1) ; #f
-
-
 
 
 
