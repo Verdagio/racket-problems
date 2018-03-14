e0e12c2e00f1e954df24bf92e3be4f674af01a23
Verdagio <dverdagio@gmail.com>
2018-03-14 13:46:01 +0000

    updated collatz conjecture readme description

--- sumListZero.rkt
+++ sumListZero.rkt
@@ -3,20 +3,36 @@
 
 ; Write a function sublsum in Racket that takes a list (of integers) as input and returns
 ; a list of sublists of it that sum to zero. For this problem, you can use the
-; combinations built-in function. Note the order of the sublists and their elements
-; doesn’t matter.
+; combinations built-in function. 
 
-(define (sublsum l)
-  )
+; take in a list,
+; check if the lowest value is less than zero
+; if it is, then check if any combinations within'
+; the list will sum to 0
+; otherwise return a null list
+
+
+(define (sublsum li)
+  (cond
+    (< 0 (getMin li)(combinations li))
+    (> 0 (getMin li) '())))
+
 
 
  
 ; reference: https://stackoverflow.com/questions/47665879/finding-minimum-value-more-efficiently-in-racket
 (define (getMin lst)
-    (cond 
+    (cond
       ((null? (cdr lst)) (car lst))
       ((< (car lst) (getMin (cdr lst))) (car lst))
       (else 
-        (getMin (cdr lst)))) )
+        (getMin (cdr lst)))))
 
-(minim (list 1 2 3 4 -5))
+(define (inverse n)
+  (* -1 n))
+
+(define (sum l)
+  (cond (null? (cdr l))
+  (+ (car l) (sum (cdr l)))))
+
+(sublsum (list 1 2 3 4 5))
