44c0f2a5aed28d874562fd25f1eed5dcd20a4673
Verdagio <dverdagio@gmail.com>
2018-03-20 14:28:22 +0000

    added hamming distance

---
+++ hammingDistance.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Write a functiono hamming-distance in Racket that takes two lists and returns
+; number of positions in which they differ.
+
+
+(define n 0)
+(define (hamming-distance l m) ; define function which takes 2 lists 
+  (if (not (pair? l)) ; exit statement if the car of l has a corresponding cdr of l which is a list not equal to null
+      n ; return the distance
+      (if (= (car l) (car m)) (hamming-distance (cdr l) (cdr m)) ; if the value of each list is the same continue the recursive call without incrementing 
+      (+ 1 (hamming-distance (cdr l) (cdr m)))))) ; otherwise they are different meaning we increment the distance by 1.
+
+(hamming-distance (list 1 1 1 1 0 0 0 0) (list  1 0 1 0 1 1 1 0))
+(hamming-distance (list 1 1 1 1 0 0 0 0) (list  1 1 1 1 0 0 0 0))
+(hamming-distance (list 1 1 1 1 0 0 0 0) (list  0 0 0 0 1 1 1 1))
